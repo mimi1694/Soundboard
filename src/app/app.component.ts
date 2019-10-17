97b60452a36ffd49b833e8ef64e81b7228962897
Mimi Scott <mimiscott94@gmail.com>
2019-10-16 21:58:02 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soundboard',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent { }
