7c2ec21cf4e1c8a5a543822f4088fa994e4b92d7
Mimi Scott <mimiscott94@gmail.com>
2019-12-04 19:57:20 -0500

    added default angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SoundboardModule } from './soundboard/soundboard.module';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     RouterModule,
     AppRoutingModule,
-    SoundboardModule
+    SoundboardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
